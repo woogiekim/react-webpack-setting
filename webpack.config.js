353cc8a6fd73b0a0d5c1c8358d6168ae8f2a9088
Woogie Kim <mdir2kor@gmail.com>
2022-07-12 23:56:59 +0900

    Hello World

--- webpack.config.js
+++ webpack.config.js
@@ -43,7 +43,6 @@ module.exports = {
         }),
         new HtmlWebpackPlugin({
             template: './index.html',
-        }),
-        new webpack.HotModuleReplacementPlugin(),
+        })
     ],
 };
