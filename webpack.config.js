6f69a77ec632676d1fc2cc886fbcae5e4c82cca1
Woogie Kim <mdir2kor@gmail.com>
2022-07-13 00:04:16 +0900

    Eslint, Prettier 설정 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,47 @@
-const webpack = require('webpack');
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const prod = process.env.NODE_ENV === 'production';
+const prod = process.env.NODE_ENV === "production";
 
 module.exports = {
-    mode: prod ? 'production' : 'development',
-    devtool: prod ? 'hidden-source-map' : 'source-map',
+  mode: prod ? "production" : "development",
+  devtool: prod ? "hidden-source-map" : "source-map",
 
-    entry: './index.tsx',
+  entry: "./index.tsx",
 
-    resolve: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-    },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
 
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: ['babel-loader', 'ts-loader'],
-            },
-        ],
-    },
-
-    output: {
-        path: path.join(__dirname, '/dist'),
-        filename: '[name].[contenthash].js',
-    },
-
-    devServer: {
-        historyApiFallback: true,
-        // inline: true,
-        port: 3000,
-        hot: true,
-        // publicPath: '/',
-
-    },
-
-    plugins: [
-        new webpack.ProvidePlugin({
-            React: 'react',
-        }),
-        new HtmlWebpackPlugin({
-            template: './index.html',
-        })
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: ["babel-loader", "ts-loader"],
+      },
     ],
+  },
+
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "[name].[contenthash].js",
+  },
+
+  devServer: {
+    historyApiFallback: true,
+    // inline: true,
+    port: 3000,
+    hot: true,
+    // publicPath: '/',
+  },
+
+  plugins: [
+    new webpack.ProvidePlugin({
+      React: "react",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ],
 };
