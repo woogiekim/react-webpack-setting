ea01388b63128506e0dea86b24cf1904a73e129c
Woogie Kim <mdir2kor@gmail.com>
2022-07-12 23:46:37 +0900

    Webpack 설정 추가

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const prod = process.env.NODE_ENV === 'production';
+
+module.exports = {
+    mode: prod ? 'production' : 'development',
+    devtool: prod ? 'hidden-source-map' : 'source-map',
+
+    entry: './index.tsx',
+
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: ['babel-loader', 'ts-loader'],
+            },
+        ],
+    },
+
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: '[name].[contenthash].js',
+    },
+
+    devServer: {
+        historyApiFallback: true,
+        // inline: true,
+        port: 3000,
+        hot: true,
+        // publicPath: '/',
+
+    },
+
+    plugins: [
+        new webpack.ProvidePlugin({
+            React: 'react',
+        }),
+        new HtmlWebpackPlugin({
+            template: './index.html',
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+};
