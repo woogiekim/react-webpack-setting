cbd21ffdbf5ee585ab3941e907985927a3706243
Woogie Kim <mdir2kor@gmail.com>
2022-07-14 20:03:04 +0900

    React Dom 설정

--- App.tsx
+++ App.tsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import Home from './src/page/Home';
+import Test from "./src/page/Test";
+
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+} from "react-router-dom";
 
 export default function App() {
-	return <div>Hello World</div>;
+    return (
+        <BrowserRouter>
+            {/* ㅍㅔ이지 변경시, Routes 내에서 스위칭이 이루어짐 */}
+            <Routes>
+                <Route path="/">
+                    <Route path="home" element={<Home/>}/>
+                    <Route path="test" element={<Test/>}/>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
